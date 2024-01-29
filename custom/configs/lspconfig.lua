bc76349471871afa9b493e3fdc043327e5832678
Ansyn <AnsynNeuhaus@gmail.com>
2024-01-28 20:37:25 -0800

    working Lua Config

---
+++ custom/configs/lspconfig.lua
@@ -0,0 +1,30 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod",".git"),
+  settings = {
+    completeUnimported = true,
+    usePlaceholders = true,
+    analyses = {
+      unusedparams = true,
+    },
+  },
+}
+
+
+lspconfig.omnisharp.setup {
+  -- Sauce: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#omnisharp
+  cmd = { "dotnet", "C:/Users/Ansyn Neuhaus/AppData/Local/nvim-data/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  -- Sauce to figure out: https://github.com/Hoffs/omnisharp-extended-lsp.nvim
+}
+
